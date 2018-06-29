eee274dc0ac5bade45cd89b61611df4a3a63d88b
Gaston Martres <gastonmartres@gmail.com>
2018-06-29 15:54:46 -0300

    Mas pruebas

--- index.php
+++ index.php
@@ -3,8 +3,5 @@ printf("Welcome to the Matrix: %s...", $_SERVER['REMOTE_ADDR']);
 
 printf("Or did you mean: %s...?", $_SERVER['HTTP_X_FORWARDED_FOR']);
 
-printf("Perhaps %s...?", $_SERVER['HTTP_CLIENT_IP']);
-
-print_r($_SERVER);
-
+printf("Hello, I'm %s...",$_SERVER['UNIQUE_ID']);
 ?>
