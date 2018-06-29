7757a1aa163f1b7f6848f4dde8a503f5cd280a47
Gaston Martres <gastonmartres@gmail.com>
2018-06-29 15:50:31 -0300

    Test Openshift 3

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
-
 printf("Welcome to the Matrix: %s...", $_SERVER['REMOTE_ADDR']);
 
+printf("Or did you mean: %s...?", $_SERVER['HTTP_X_FORWARDED_FOR']);
+
+printf("Perhaps %s...?", $_SERVER['HTTP_CLIENT_IP']);
+
+print_r($_SERVER);
 
 ?>
