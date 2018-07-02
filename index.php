a3546207e4abbe419382796b5f3cc2508d8bb29d
Gaston Martres <gastonmartres@gmail.com>
2018-07-02 17:27:37 -0300

    Prueba con mysql

--- index.php
+++ index.php
@@ -4,4 +4,15 @@ printf("Welcome to the Matrix: %s...", $_SERVER['REMOTE_ADDR']);
 printf("Or did you mean: %s...?", $_SERVER['HTTP_X_FORWARDED_FOR']);
 
 printf("Hello, I'm %s...",$_SERVER['UNIQUE_ID']);
+
+$link = mysqli_connect("mysql","gmartres","asdf1234","sampledb");
+
+if (!link){
+	echo "No se pudo conectar con la DB: " .PHP_EOL;
+} else {
+	echo "Conexion con la DB exitosa";
+}
+
+
+
 ?>
