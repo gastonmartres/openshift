abb542efc59e50c3077d6d86422eee8a643ab428
gastonmartres <31313527+gastonmartres@users.noreply.github.com>
2019-12-27 15:35:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,14 +5,7 @@ printf("Or did you mean: %s...?", $_SERVER['HTTP_X_FORWARDED_FOR']);
 
 printf("Hello, I'm %s...",$_SERVER['UNIQUE_ID']);
 
-$link = mysqli_connect($_ENV['MYSQL_HOST'],$_ENV['MYSQL_USER'],$_ENV['MYSQL_PASSWORD'],$_ENV['MYSQL_DB']);
-
-if (!link){
-	echo "No se pudo conectar con la DB: " .PHP_EOL;
-} else {
-	echo "Conexion con la DB exitosa";
-}
-
+phpinfo();
 
 
 ?>
