35d88f4c76131a9c95f336379863380559a47a0e
Gaston Martres <gastonmartres@gmail.com>
2020-07-01 10:35:54 -0300

    Cambios esteticos

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <html>
-<head>
-<title>Pagina de prueba en PHP</title>
-<body>
-<h1>Welcome to the matrix <?=$_SERVER['REMOTE_ADDR'];?></h1>
-<br>
-<h2>Or did you mean <?=$_SERVER['HTTP_X_FORWARDED_FOR'];?><h2>
-<br>
-<br><h3>Hello I'm <?=$_SERVER['UNIQUE_ID'];?></h3>
-<br>
-</body>
+    <head>
+        <title>Pagina de prueba en PHP</title>
+    </head>
+    <body>
+        <h1>Welcome to the matrix <?=$_SERVER['REMOTE_ADDR'];?></h1>
+        <br>
+        <h2>Or did you mean <?=$_SERVER['HTTP_X_FORWARDED_FOR'];?><h2>
+        <br>
+        <br><h3>Hello I'm <?=$_SERVER['UNIQUE_ID'];?></h3>
+        <br>
+    </body>
 </html>
