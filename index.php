532b8c430bd6e86fa94599c604bcc547b5a9e437
Gaston Martres <gastonmartres@gmail.com>
2020-07-01 10:28:11 -0300

    Cambios esteticos

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
 printf("Welcome to the Matrix: %s...", $_SERVER['REMOTE_ADDR']);
-
+printf("\n");
 printf("Or did you mean: %s...?", $_SERVER['HTTP_X_FORWARDED_FOR']);
-
+printf("\n");
 printf("Hello, I'm %s...",$_SERVER['UNIQUE_ID']);
-
-phpinfo();
-
-
+printf("\n");
 ?>
